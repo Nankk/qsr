fc5ae653a69d6ac22bc0af57fc31515d71c6466f
Nankk <toc32768@gmail.com>
2019-10-12 22:34:27 +0900

    :bug: Fixed sort-js reloading
    SortJSのインスタンス生成がshadow-cljsのリロード対象になっておらず、Warningを出す
    だけに留まらず他コンポーネントの再描画も行われなくなっていたので修正。

--- src/qsr/core.cljs
+++ src/qsr/core.cljs
@@ -11,25 +11,28 @@
   (when config/debug?
     (println "dev mode")))
 
-(defn ^:dev/after-load mount-root []
-  (re-frame/clear-subscription-cache!)
-  (reagent/render [views/main-panel]
-                  (.getElementById js/document "app")))
-
-(defn sortable-init []
+(defn sortable-setup []
+  ;; This function must be re-loaded in the develop build *after* reagent re-render
+  (println "sortable-setup loaded.")
   (Sortable. (.getElementById js/document "item-list")
              (clj->js {:group "g1"
                        :animation 50
                        :delay 150
                        :delayOnTouchOnly true
                        :onEnd (fn [e]
-                                  (let [old (. e -oldIndex)
-                                        new (. e -newIndex)
-                                        from-to [old new]]
-                                    (re-frame/dispatch-sync [::events/on-manually-sorted from-to])))})))
+                                (let [old (. e -oldIndex)
+                                      new (. e -newIndex)
+                                      from-to [old new]]
+                                  (re-frame/dispatch-sync [::events/on-manually-sorted from-to])))})))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app"))
+  (sortable-setup))
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
   (mount-root)
-  (sortable-init))
+  )
