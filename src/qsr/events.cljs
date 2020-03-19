12f4f6b4749d9af61b0f62e66412d4bfdca3d7be
Nankk <toc32768@gmail.com>
2020-03-19 13:38:35 +0900

    :shirt: Clean keywords' position in events and subs

--- src/qsr/events.cljs
+++ src/qsr/events.cljs
@@ -4,14 +4,12 @@
    [qsr.db :as db]
    [qsr.util :as util]))
 
-(rf/reg-event-db
-  ::initialize-db
+(rf/reg-event-db ::initialize-db
   (fn [_ _]
     (println db/default-db)
     db/default-db))
 
-(rf/reg-event-db
-  ::select-item
+(rf/reg-event-db ::select-item
   (fn [db [_ item]]
     (let [items (db :items)
           select-idx (first (keep-indexed #(when (= (%2 :id) (item :id)) %1) items))
@@ -19,23 +17,19 @@
                                             (vec (map #(assoc % :selected? false) item))))]
       (update-in unselected-db [:items select-idx :selected?] #(constantly true)))))
 
-(rf/reg-event-db
-  ::set-items
+(rf/reg-event-db ::set-items
   (fn [db [_ items]]
     (assoc db :items items)))
 
-(rf/reg-event-db
-  ::set-sort-by
+(rf/reg-event-db ::set-sort-by
   (fn [db [_ by]]
     (assoc db :sort-by by)))
 
-(rf/reg-event-db
-  ::set-sort-order
+(rf/reg-event-db ::set-sort-order
   (fn [db [_ order]]
     (assoc db :sort-order order)))
 
-(rf/reg-event-db
-  ::sort-items
+(rf/reg-event-db ::sort-items
   (fn [db _]
     (let [sorted-items (sort-by (db :sort-by) (db :items))]
       (assoc db :items
@@ -43,21 +37,18 @@
                :ascending (vec sorted-items)
                :descending (vec (reverse sorted-items)))))))
 
-(rf/reg-event-db
-  ::on-manually-sorted
+(rf/reg-event-db ::on-manually-sorted
   (fn [db [_ from-to]]
     (let [from (from-to 0)
           to (from-to 1)]
       (-> db
           (update :items #(vec (util/shift-from-to % from to)))))))
 
-(rf/reg-event-db
-  ::will-reflect-slide
+(rf/reg-event-db ::will-reflect-slide
   (fn [db _]
     (assoc db :reflecting? true)))
 
-(rf/reg-event-db
-  ::did-reflect-slide
+(rf/reg-event-db ::did-reflect-slide
   (fn [db [_ res]]
     (println "Received response from php API:")
     (println res)
--- src/qsr/subs.cljs
+++ src/qsr/subs.cljs
@@ -3,27 +3,22 @@
    [re-frame.core :as rf]
    ))
 
-(rf/reg-sub
-  ::name
+(rf/reg-sub ::name
   (fn [db _]
     (db :name)))
 
-(rf/reg-sub
-  ::items
+(rf/reg-sub ::items
   (fn [db _]
     (db :items)))
 
-(rf/reg-sub
-  ::sort-by
+(rf/reg-sub ::sort-by
   (fn [db _]
     (db :sort-by)))
 
-(rf/reg-sub
-  ::sort-order
+(rf/reg-sub ::sort-order
   (fn [db _]
     (db :sort-order)))
 
-(rf/reg-sub
-  ::reflecting?
+(rf/reg-sub ::reflecting?
   (fn [db _]
     (db :reflecting?)))
